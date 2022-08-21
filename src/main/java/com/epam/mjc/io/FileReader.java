2a79e5312837510b6fe0adf1784998398886232e
Akmal Ismoilov <master80098@gmail.com>
2022-08-21 13:11:47 +0500

    Replaced "\s" with " ", because it was causing compile time error on autocode

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
-public class FileReader {
+public class FileReader
+{
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file)
+    {
+        Profile profile;
+
+        try (InputStream inputStream = new FileInputStream(file))
+        {
+            //Read file and save lines
+            String fileContent = new String(inputStream.readAllBytes());
+            String[] lines = fileContent.split("\r\n");
+
+            //Parse lines
+            String name = lines[0].split(" ")[1];
+            Integer age = Integer.parseInt(lines[1].split(" ")[1]);
+            String email = lines[2].split(" ")[1];
+            Long phone = Long.parseLong(lines[3].split(" ")[1]);
+
+            //Create profile
+            profile = new Profile(name, age, email, phone);
+        } catch (IOException e)
+        {
+            profile = null;
+        }
+
+        return profile;
     }
 }
